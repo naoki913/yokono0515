866d1c148b180528e947eb23b86472f06216b32d
naoki913 <mr.kanoi3190@gmail.com>
2017-05-28 01:48:47 +0900

    プログラム更新

--- Block_2/src/ofApp.cpp
+++ Block_2/src/ofApp.cpp
@@ -5,18 +5,21 @@
 void ofApp::setup(){
 	ofSetFrameRate(60);
 	ofSetBackgroundColor(255, 255, 255);
+	gameClear.load("images/gameclear.png");
 	
 
 	//ボールの初期位置
-	ballPosition = ofVec2f(ofGetWidth() / 2, ofGetHeight() / 5 * 4);
+	ballPosition = ofVec2f(500,600);
 	//ボールの速さ
-	float vx = ofRandom(-10, 10);
-	float vy = ofRandom(-10, -8);
+	int x = ofRandom(0, 2);
+	int y = ofRandom(0, 5);
+	float vx = speedx[x];
+	float vy = speedy[y];
 	ballSpeed = ofVec2f(vx, vy);
 	//ボールの色
-	ballColor = ofColor(0, 0, 0);
+	ballColor = ofColor(r3, g2, b1);
 	//ボールの半径
-	ballRadius = 5;
+	ballRadius = 7;
 }
 
 //--------------------------------------------------------------
@@ -24,18 +27,22 @@ void ofApp::update(){
 	//ボールに速さを与える
 	ballPosition += ballSpeed;
 	//反射
-	if (ballPosition.x <= 0) {
+	if (ballPosition.x < 20) {
+		ballPosition.x = 20;
 		ballSpeed.x *= -1;
 	}
-	if (ballPosition.x >= ofGetWidth()) {
+	if (ballPosition.x > 1000) {
+		ballPosition.x = 1000;
 		ballSpeed.x *= -1;
 	}
-	if (ballPosition.y <= 0) {
+	if (ballPosition.y < 0) {
+		ballPosition.y = 0;
 		ballSpeed.y *= -1;
 	}
 
 	//バーで反射
-	if (ballPosition.x > mouseX - barWidth / 2 && mouseX - barWidth / 2 + barWidth > ballPosition.x&&ballPosition.y> ofGetHeight() / 5 * 4 + 10&&ballPosition.y<ofGetHeight() / 5 * 4 + 10+barHeight) {
+	if (ballPosition.x > mouseX - barWidth / 2 && mouseX - barWidth / 2 + barWidth > ballPosition.x&&ballPosition.y> barPositionY &&ballPosition.y<barPositionY+barHeight) {
+		ballPosition.y = barPositionY;
 		ballSpeed.y *= -1;
 	}
 
@@ -44,74 +51,302 @@ void ofApp::update(){
 	
 	
 	
-	/*if (ballPosition.x<num1+num1&&ballPosition.x>num1&&ballPosition.y<num1+num1&&ballPosition.y>num1) {
-		ballSpeed.y *= -1;
-		num1 = 0;
-	}  */  
-
+	//ブロック反射
 	//1
-	if (ballPosition.x<numx1 + width1&&ballPosition.x>numx1&&ballPosition.y<numy1 + height1&&ballPosition.y>numy1) {
-		ballSpeed.y *= -1;
-		numx1 = 0;
-		numy1 = 0;
-		width1 = 0;
-		height1 = 0;
+	if (ballPosition.x<numx1 + width1&&ballPosition.x>numx1) {
+		if (ballPosition.y == numy1 ) {
+			ballPosition.y = 100;
+			ballSpeed.y *= -1;
+			numx1 = 0;
+			numy1 = 0;
+			width1 = 0;
+			height1 = 0;
+		}
+		if ( ballPosition.y == numy1 + height1) {
+			ballPosition.y = 130;
+			ballSpeed.y *= -1;
+			numx1 = 0;
+			numy1 = 0;
+			width1 = 0;
+			height1 = 0;
+		}
+	}
+	if (ballPosition.y<numy1 + height1&&ballPosition.y>numy1) {
+		if (ballPosition.x == numx1) {
+			ballPosition.x = 100;
+			ballSpeed.x *= -1;
+			numx1 = 0;
+			numy1 = 0;
+			width1 = 0;
+			height1 = 0;
+		}
+		if ( ballPosition.x == numx1 + width1) {
+			ballPosition.x = 250;
+			ballSpeed.x *= -1;
+			numx1 = 0;
+			numy1 = 0;
+			width1 = 0;
+			height1 = 0;
+		}
 	}
 	//2
-	if (ballPosition.x<numx2 + width2&&ballPosition.x>numx2&&ballPosition.y<numy2 + height2&&ballPosition.y>numy2) {
-		ballSpeed.y *= -1;
-		numx2 = 0;
-		numy2 = 0;
-		width2 = 0;
-		height2 = 0;
+	if (ballPosition.x<numx2 + width2&&ballPosition.x>numx2) {
+		if (ballPosition.y == numy2) {
+			ballPosition.y = 300;
+			ballSpeed.y *= -1;
+			numx2 = 0;
+			numy2 = 0;
+			width2 = 0;
+			height2 = 0;
+		}
+		if (ballPosition.y == numy2 + height2) {
+			ballPosition.y = 330;
+			ballSpeed.y *= -1;
+			numx2 = 0;
+			numy2 = 0;
+			width2 = 0;
+			height2 = 0;
+		}
+	}
+	if (ballPosition.y<numy2 + height2&&ballPosition.y>numy2) {
+		if (ballPosition.x == numx2) {
+			ballPosition.x = 100;
+			ballSpeed.x *= -1;
+			numx2 = 0;
+			numy2 = 0;
+			width2 = 0;
+			height2 = 0;
+		}
+		if (ballPosition.x == numx2 + width2) {
+			ballPosition.x = 250;
+			ballSpeed.x *= -1;
+			numx2 = 0;
+			numy2 = 0;
+			width2 = 0;
+			height2 = 0;
+		}
 	}
 	//3
-	if (ballPosition.x<numx3 + width3&&ballPosition.x>numx3&&ballPosition.y<numy3 + height3&&ballPosition.y>numy3) {
-		ballSpeed.y *= -1;
-		numx3 = 0;
-		numy3 = 0;
-		width3 = 0;
-		height3 = 0;
+	if (ballPosition.x<numx3 + width3&&ballPosition.x>numx3) {
+		if (ballPosition.y == numy3) {
+			ballPosition.y = 200;
+			ballSpeed.y *= -1;
+			numx3 = 0;
+			numy3 = 0;
+			width3 = 0;
+			height3 = 0;
+		}
+		if (ballPosition.y == numy3 + height3) {
+			ballPosition.y = 230;
+			ballSpeed.y *= -1;
+			numx3 = 0;
+			numy3 = 0;
+			width3 = 0;
+			height3 = 0;
+		}
+	}
+	if (ballPosition.y<numy3 + height3&&ballPosition.y>numy3) {
+		if (ballPosition.x == numx3) {
+			ballPosition.x = 250;
+			ballSpeed.x *= -1;
+			numx3 = 0;
+			numy3 = 0;
+			width3 = 0;
+			height3 = 0;
+		}
+		if (ballPosition.x == numx3 + width3) {
+			ballPosition.x = 400;
+			ballSpeed.x *= -1;
+			numx3 = 0;
+			numy3 = 0;
+			width3 = 0;
+			height3 = 0;
+		}
 	}
 	//4
-	if (ballPosition.x<numx4 + width4&&ballPosition.x>numx4&&ballPosition.y<numy4 + height4&&ballPosition.y>numy4) {
-		ballSpeed.y *= -1;
-		numx4 = 0;
-		numy4 = 0;
-		width4 = 0;
-		height4 = 0;
+	if (ballPosition.x<numx4 + width4&&ballPosition.x>numx4) {
+		if (ballPosition.y == numy4) {
+			ballPosition.y = 100;
+			ballSpeed.y *= -1;
+			numx4 = 0;
+			numy4 = 0;
+			width4 = 0;
+			height4 = 0;
+		}
+		if (ballPosition.y == numy4 + height4) {
+			ballPosition.y = 130;
+			ballSpeed.y *= -1;
+			numx4 = 0;
+			numy4 = 0;
+			width4 = 0;
+			height4 = 0;
+		}
+	}
+	if (ballPosition.y<numy4 + height4&&ballPosition.y>numy4) {
+		if (ballPosition.x == numx4) {
+			ballPosition.x = 400;
+			ballSpeed.x *= -1;
+			numx4 = 0;
+			numy4 = 0;
+			width4 = 0;
+			height4 = 0;
+		}
+		if (ballPosition.x == numx4 + width4) {
+			ballPosition.x = 550;
+			ballSpeed.x *= -1;
+			numx4 = 0;
+			numy4 = 0;
+			width4 = 0;
+			height4 = 0;
+		}
 	}
 	//5
-	if (ballPosition.x<numx5 + width5&&ballPosition.x>numx5&&ballPosition.y<numy5 + height5&&ballPosition.y>numy5) {
-		ballSpeed.y *= -1;
-		numx5 = 0;
-		numy5 = 0;
-		width5 = 0;
-		height5 = 0;
+	if (ballPosition.x<numx5 + width5&&ballPosition.x>numx5) {
+		if (ballPosition.y == numy5) {
+			ballPosition.y = 300;
+			ballSpeed.y *= -1;
+			numx5 = 0;
+			numy5 = 0;
+			width5 = 0;
+			height5 = 0;
+		}
+		if (ballPosition.y == numy5 + height5) {
+			ballPosition.y = 330;
+			ballSpeed.y *= -1;
+			numx5 = 0;
+			numy5 = 0;
+			width5 = 0;
+			height5 = 0;
+		}
+	}
+	if (ballPosition.y<numy5 + height5&&ballPosition.y>numy5) {
+		if (ballPosition.x == numx5) {
+			ballPosition.x = 400;
+			ballSpeed.x *= -1;
+			numx5 = 0;
+			numy5 = 0;
+			width5 = 0;
+			height5 = 0;
+		}
+		if (ballPosition.x == numx5 + width5) {
+			ballPosition.x = 550;
+			ballSpeed.x *= -1;
+			numx5 = 0;
+			numy5 = 0;
+			width5 = 0;
+			height5 = 0;
+		}
 	}
 	//6
-	if (ballPosition.x<numx6 + width6&&ballPosition.x>numx6&&ballPosition.y<numy6 + height6&&ballPosition.y>numy6) {
-		ballSpeed.y *= -1;
-		numx6 = 0;
-		numy6 = 0;
-		width6 = 0;
-		height6 = 0;
+	if (ballPosition.x<numx6 + width6&&ballPosition.x>numx6) {
+		if (ballPosition.y == numy6) {
+			ballPosition.y = 200;
+			ballSpeed.y *= -1;
+			numx6 = 0;
+			numy6 = 0;
+			width6 = 0;
+			height6 = 0;
+		}
+		if (ballPosition.y == numy6 + height6) {
+			ballPosition.y = 230;
+			ballSpeed.y *= -1;
+			numx6 = 0;
+			numy6 = 0;
+			width6 = 0;
+			height6 = 0;
+		}
+	}
+	if (ballPosition.y<numy6 + height6&&ballPosition.y>numy6) {
+		if (ballPosition.x == numx6) {
+			ballPosition.x = 550;
+			ballSpeed.x *= -1;
+			numx6 = 0;
+			numy6 = 0;
+			width6 = 0;
+			height6 = 0;
+		}
+		if (ballPosition.x == numx6 + width1) {
+			ballPosition.x = 700;
+			ballSpeed.x *= -1;
+			numx6 = 0;
+			numy6 = 0;
+			width6 = 0;
+			height6 = 0;
+		}
 	}
 	//7
-	if (ballPosition.x<numx7 + width7&&ballPosition.x>numx7&&ballPosition.y<numy7 + height7&&ballPosition.y>numy7) {
-		ballSpeed.y *= -1;
-		numx7 = 0;
-		numy7 = 0;
-		width7 = 0;
-		height7 = 0;
+	if (ballPosition.x<numx7 + width7&&ballPosition.x>numx7) {
+		if (ballPosition.y == numy7) {
+			ballPosition.y = 100;
+			ballSpeed.y *= -1;
+			numx7 = 0;
+			numy7 = 0;
+			width7 = 0;
+			height7 = 0;
+		}
+		if (ballPosition.y == numy7 + height7) {
+			ballPosition.y = 130;
+			ballSpeed.y *= -1;
+			numx7 = 0;
+			numy7 = 0;
+			width7 = 0;
+			height7 = 0;
+		}
+	}
+	if (ballPosition.y<numy7 + height7&&ballPosition.y>numy7) {
+		if (ballPosition.x == numx7) {
+			ballPosition.x = 700;
+			ballSpeed.x *= -1;
+			numx7 = 0;
+			numy7 = 0;
+			width7 = 0;
+			height7 = 0;
+		}
+		if (ballPosition.x == numx7 + width7) {
+			ballPosition.x = 850;
+			ballSpeed.x *= -1;
+			numx7 = 0;
+			numy7 = 0;
+			width7 = 0;
+			height7 = 0;
+		}
 	}
 	//8
-	if (ballPosition.x<numx8 + width8&&ballPosition.x>numx8&&ballPosition.y<numy8 + height8&&ballPosition.y>numy8) {
-		ballSpeed.y *= -1;
-		numx8 = 0;
-		numy8 = 0;
-		width8 = 0;
-		height8 = 0;
+	if (ballPosition.x<numx8 + width8&&ballPosition.x>numx8) {
+		if (ballPosition.y == numy8) {
+			ballPosition.y = 300;
+			ballSpeed.y *= -1;
+			numx8 = 0;
+			numy8 = 0;
+			width8 = 0;
+			height8 = 0;
+		}
+		if (ballPosition.y == numy8 + height8) {
+			ballPosition.y = 330;
+			ballSpeed.y *= -1;
+			numx8 = 0;
+			numy8 = 0;
+			width8 = 0;
+			height8 = 0;
+		}
+	}
+	if (ballPosition.y<numy8 + height8&&ballPosition.y>numy8) {
+		if (ballPosition.x == numx8) {
+			ballPosition.x = 700;
+			ballSpeed.x *= -1;
+			numx8 = 0;
+			numy8 = 0;
+			width8 = 0;
+			height8 = 0;
+		}
+		if (ballPosition.x == numx8 + width8) {
+			ballPosition.x = 850;
+			ballSpeed.x *= -1;
+			numx8 = 0;
+			numy8 = 0;
+			width8 = 0;
+			height8 = 0;
+		}
 	}
 	
 	
@@ -124,17 +359,36 @@ void ofApp::draw() {
 	ofDrawCircle(ballPosition, ballRadius);
 
 	//バー
-	ofDrawRectangle(mouseX - barWidth / 2, ofGetHeight() / 5 * 4 + 10, barWidth, barHeight);
+	ofSetColor(r1, g2, b3);
+	ofDrawRectangle(mouseX - barWidth / 2, barPositionY, barWidth, barHeight);
 	//ブロック
-	//ofDrawRectangle(num1, num1, num1, num1);
+	ofSetColor(r1, g1, b1);
 	ofDrawRectangle(numx1, numy1, width1, height1);
+	ofSetColor(r1, g1, b1);
 	ofDrawRectangle(numx2, numy2, width2, height2);
+	ofSetColor(r1, g2, b2);
 	ofDrawRectangle(numx3, numy3, width3, height3);
+	ofSetColor(r1, g1, b2);
 	ofDrawRectangle(numx4, numy4, width4, height4);
+	ofSetColor(r1, g1, b2);
 	ofDrawRectangle(numx5, numy5, width5, height5);
+	ofSetColor(r2, g2, b2);
 	ofDrawRectangle(numx6, numy6, width6, height6);
+	ofSetColor(r2, g1, b1);
 	ofDrawRectangle(numx7, numy7, width7, height7);
+	ofSetColor(r2, g1, b1);
 	ofDrawRectangle(numx8, numy8, width8, height8);
+	//壁
+	ofSetColor(r2, g2, b2);
+	ofDrawRectangle(kabex1, kabey1, kabeWidth1, kabeHeight1);
+	ofDrawRectangle(kabex2, kabey2, kabeWidth2, kabeHeight2);
+
+	if (numx1 == 0 && numx2 == 0 && numx3 == 0 && numx4 == 0 && numx5 == 0 && numx6 == 0 && numx7 == 0 && numx8 == 0) {
+		ballRadius = 0;
+		ofSetColor(255, 255, 255);
+		float wave = sin(ofGetElapsedTimef());
+		gameClear.draw(180 + (wave * 100), 80);
+	}
 }
 
 //--------------------------------------------------------------
--- Block_2/src/ofApp.h
+++ Block_2/src/ofApp.h
@@ -27,48 +27,70 @@ class ofApp : public ofBaseApp{
 		ofColor ballColor;
 
 		float ballRadius;
-		float barWidth = 100;
+		float barWidth = 1000;
 		float barHeight = 20;
+		float barPositionY = 700;
 
+		//カラフル
+		float r1 = ofRandom(0, 255) , g1 = ofRandom(0, 255) , b1 = ofRandom(0, 255);
+		float r2 = ofRandom(0, 255) , g2 = ofRandom(0, 255) , b2 = ofRandom(0, 255);
+		float r3 = ofRandom(0, 255) , g3 = ofRandom(0, 255) , b3 = ofRandom(0, 255);
+		
 		//ブロック１
 		float numx1 = 100;
 		float numy1 = 100;
-		float height1 = 25;
+		float height1 = 30;
 		float width1 = 150;
 		//ブロック２
 		float numx2 = 100;
 		float numy2 = 300;
-		float height2 = 25;
+		float height2 = 30;
 		float width2 = 150;
 		//ブロック3
 		float numx3 = 250;
 		float numy3 = 200;
-		float height3 = 25;
+		float height3 = 30;
 		float width3 = 150;
 		//ブロック4
 		float numx4 = 400;
 		float numy4 = 100;
-		float height4 = 25;
+		float height4 = 30;
 		float width4 = 150;
 		//ブロック5
 		float numx5 = 400;
 		float numy5 = 300;
-		float height5 = 25;
+		float height5 = 30;
 		float width5 = 150;
 		//ブロック6
 		float numx6 = 550;
 		float numy6 = 200;
-		float height6 = 25;
+		float height6 = 30;
 		float width6 = 150;
 		//ブロック7
 		float numx7 = 700;
 		float numy7 = 100;
-		float height7 = 25;
+		float height7 = 30;
 		float width7 = 150;
 		//ブロック8
 		float numx8 = 700;
 		float numy8 = 300;
-		float height8 = 25;
+		float height8 = 30;
 		float width8 = 150;
 		
+		//ボールの速さ、配列
+		int speedy[3] = { -2,-5,-10 };
+		int speedx[6] = { -10, -5,-2,2,5,10 };
+
+		//壁１
+		float kabex1 = 0;
+		float kabey1 = 0;
+		float kabeWidth1 = 20;
+		float kabeHeight1 = 800;
+		//壁２
+		float kabex2 = 1000;
+		float kabey2 = 0;
+		float kabeWidth2 = 30;
+		float kabeHeight2 = 800;
+
+		ofImage gameClear;
 };
